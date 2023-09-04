06c1fa18e18e0c920fc2d4215bbd630526357b77
time.nt.wait@gmail.com <time.nt.wait@gmail.com>
2023-09-04 08:47:13 +0300

    Парсер https://torgi.gov.ru/ Добавлены: - контроль окончания объектов в страницах поиска - возможность удаления загруженных json-файлов

--- back/parser/Torgi/get_json_api.py
+++ back/parser/Torgi/get_json_api.py
@@ -1,5 +1,6 @@
 import os
 from os.path import isdir
+import shutil
 
 from datetime import date
 from tqdm import tqdm
@@ -52,7 +53,9 @@ class APIDownload:
 
 
 class TorgiGenerateURL:
-    def __init__(self):
+    def __init__(self, path: str = ''):
+        self.path = path
+        self.api_download = APIDownload(path=self.path)
         self.list_objects_url = {}
         self.objects_url = {}
 
@@ -94,10 +97,11 @@ class TorgiGenerateURL:
         objects = []
         for fname in os.listdir(json_path):
             if fname.split('.')[-1] == "json":
-                with open(json_path + '/' + fname, 'r', encoding='utf-8') as fhandler:
-                    page_json = json.load(fhandler)
-                    for obj in page_json['content']:
-                        objects.append(obj['id'])
+                if os.path.getsize(json_path + '/' + fname) > 0:
+                    with open(json_path + '/' + fname, 'r', encoding='utf-8') as fhandler:
+                        page_json = json.load(fhandler)
+                        for obj in page_json['content']:
+                            objects.append(obj['id'])
         return objects
 
     def get_objects(self, json_path: str) -> pd.DataFrame:
@@ -118,35 +122,55 @@ class TorgiGenerateURL:
                     df = pd.concat([df, pd.Series(object_json).to_frame().T])
         return df
 
+    def get_data(self, batch_size: int = 10, pages: int = 10, cat_code: int = 2, clear_pages: bool = True,
+                 clear_objects: bool = False, workers: int = 5) -> pd.DataFrame:
+        """
+        :param batch_size: кол-во торгов на одной странице
+        :param pages: кол-во страниц
+        :param cat_code: номер категории, для земельных участков = 2
+        :param clear_pages: удалять ли json-файлы по страницам
+        :param clear_object: удалять ли json-файлы по самим объектам
+        :param workers: кол-во паллельных процессов
+        :return: pandas.DataFrame - датафрейм с данными по торгам
+        """
+
+        # генерируем URL страниц списков объектов
+        url_dict = self.generate_url_list_objects(batch_size=batch_size, pages=pages, cat_code=cat_code)
+        print(f"Выбрано {len(url_dict)} страниц с торгами")
+
+        # Скачиываем страницы списков объектов
+        path_list_objects = self.path + 'list_obj_jsons'
+        self.api_download.download_all(url_dict, sub_path=path_list_objects, workers=workers)
+
+        # Определяем id торгов на основе скачанных json page
+        objects_id = self.get_objects_id(json_path=path_list_objects)
+        print(f"Выбрано {len(objects_id)} объектов")
+
+        # генерируем URL для каждого объекта
+        objects_url = self.generate_url_ad_torgi(objects_id)
+        path_objects = self.path + 'torgi_json'
+        self.api_download.download_all(objects_url, sub_path=path_objects, workers=workers)
+
+        print(f'download {len(objects_url)} objects from {len(url_dict)} pages')
+        # Формируем из json объектов DataFrame
+        df_torgi = self.get_objects(json_path=path_objects)
+
+        if clear_pages:
+            shutil.rmtree(path_list_objects)
+        if clear_objects:
+            shutil.rmtree(path_objects)
+
+        return df_torgi
+
 
 if __name__ == "__main__":
     # Пример использования парсера сайта https://torgi.gov.ru/
     PATH = ''
 
-    api_download = APIDownload(path=PATH)
-    torgi_urls = TorgiGenerateURL()
+    torgi_urls = TorgiGenerateURL(path=PATH)
+    df_torgi = torgi_urls.get_data(batch_size=1000, pages=1000, cat_code=2, clear_pages=False, clear_objects=False,
+                                   workers=5)
 
-    # генерируем URL страниц списков объектов
-    url_dict = torgi_urls.generate_url_list_objects(batch_size=10, pages=15, cat_code=2)
-    print(f"Выбрано {len(url_dict)} страниц с торгами")
-
-    # Скачиываем страницы списков объектов
-    path_list_objects = PATH + 'list_obj_jsons'
-    api_download.download_all(url_dict, sub_path=path_list_objects, workers=5)
-
-    # Определяем id торгов на основе скачанных json page
-    objects_id = torgi_urls.get_objects_id(json_path=path_list_objects)
-    print(f"Выбрано {len(objects_id)} объектов")
-
-    # генерируем URL для каждого объекта
-    objects_url = torgi_urls.generate_url_ad_torgi(objects_id)
-    path_objects = PATH + 'torgi_json'
-    api_download.download_all(objects_url, sub_path=path_objects, workers=5)
-
-    # Формируем из json объектов DataFrame
-    df_torgi = torgi_urls.get_objects(json_path=path_objects)
+    PATH_DATASET = PATH  # + "../../../datasets/"
     if df_torgi is not None:
-        PATH_DATASET = PATH  # + "../../../datasets/"
         df_torgi.set_index('id').to_csv(PATH_DATASET + "torgi_dataset.csv", index_label='id')
-
-    print(f'download {len(objects_url)} objects from {len(url_dict)} pages')
